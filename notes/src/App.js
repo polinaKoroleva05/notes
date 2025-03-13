e46a5881d372820ae305a67ab6126ccd5fb403a1
Test User <polina.koroleva05@mail.ru>
2025-03-13 17:33:22 +0300

    local storage

--- notes/src/App.js
+++ notes/src/App.js
@@ -4,15 +4,25 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Note from './components/Note';
 import ListOfNotes from './components/ListOfNotes';
 import AddNote from './components/AddNote';
+import testData from './testData/testData';
+import { MdAdd } from "react-icons/md";
 
 export default function App() {
     const [listNotes, changeListNotes] = useState([])
     const [openedNoteId, changeOpenedNoteId] = useState(-1)
     const navigate = useNavigate();
 
-    const getApiData = async () => {
-        const response = await fetch("data/notes.json").then(res => res.json());
-        changeListNotes(response);
+    const getApiData = () => {
+        if(!localStorage.getItem('notes') || localStorage.getItem('notes')===undefined){
+            localStorage.setItem('notes', JSON.stringify([testData]));
+        }
+        console.log('testdata', testData)
+        const notesFromStorage = JSON.parse(localStorage.getItem('notes'));
+        changeListNotes(notesFromStorage);
+    }
+
+    const saveApiData = (newListNotes) => {
+        localStorage.setItem('notes', JSON.stringify(newListNotes));
     }
 
     useEffect(() => {
@@ -23,13 +33,24 @@ export default function App() {
         listNotes.splice(openedNoteId, 1);
         changeListNotes(listNotes);
         changeOpenedNoteId(-1);
+        saveApiData();
     }
 
     const changeNote = (newNote) => {
         listNotes.splice(openedNoteId, 1);
         newNote.date = (new Date()).toDateString();
-        changeListNotes([...listNotes, newNote]);
+        let newListNotes = [...listNotes, newNote];
+        changeListNotes(newListNotes);
         changeOpenedNoteId(listNotes.length);
+        saveApiData(newListNotes);
+    }
+
+    const addNote = (newNote) => {
+        newNote.date = (new Date()).toDateString();
+        let newListNotes = [...listNotes, newNote];
+        changeListNotes(newListNotes);
+        changeId(listNotes.length);
+        saveApiData(newListNotes);
     }
 
     const changeId = (id) => {
@@ -37,15 +58,9 @@ export default function App() {
         navigate('/');
     }
 
-    const addNote = (newNote) => {
-        console.log('here')
-        newNote.date = (new Date()).toDateString();
-        changeListNotes([...listNotes, newNote]);
-        changeOpenedNoteId(listNotes.length);
-    }
     return (
         <div className='App'>
-            <button onClick={()=>navigate('/addNote')}>доб</button>
+            <button className='addButton' onClick={()=>navigate('/addNote')}><MdAdd/></button>
             <div className='container'>
                 <ListOfNotes listNotes={listNotes} onChangeId={changeId} />
                 <Routes>
